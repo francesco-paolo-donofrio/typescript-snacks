b07033372ac070e1281a6a02197dd2d699acd83e
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-28 20:37:38 +0200

    Setting up the Development Environment + Debugging from VSCode

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let myConst = 1;
+myConst = 2;
+console.log(myConst);
+let myConst2 = 1;
+myConst2 = 2;
+console.log(myConst2);
+//# sourceMappingURL=index.js.map
--- src/dist/index.js
+++
@@ -1,6 +0,0 @@
-var myConst = 1;
-myConst = 2;
-console.log(myConst);
-var myConst2 = 1;
-myConst2 = 2;
-console.log(myConst2);
