577de8886f03b7749f5303567434b4504496d52f
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-29 18:49:37 +0200

    Debugged some Typescript Fundamentals

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,7 @@ console.log(myConst);
 let myConst2 = 1;
 myConst2 = 2;
 console.log(myConst2);
+let course = "TypeScript";
+course = "2";
+console.log(course);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Debugging Typescript application
+
+// Example Fundamental Types
+
 let myConst: number = 1;
 myConst = 66;
 
@@ -6,4 +10,10 @@ console.log(myConst);
 let myConst2: number = 1;
 myConst2 = 2;
 
-console.log(myConst2);
+console.log(myConst2); 
+
+let course: string = "TypeScript";
+course = "2";
+
+console.log(course);
+
