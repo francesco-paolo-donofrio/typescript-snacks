a7658a58e66f59357d77fac3510524af369979a9
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-03 23:58:17 +0200

    Snack 2 - Return Negative

--- dist/index.js
+++ dist/index.js
@@ -10,4 +10,18 @@ function isEvenOrOdd(number) {
 ;
 isEvenOrOdd(2);
 isEvenOrOdd(3);
+function makeNegative(number) {
+    if (number < 0) {
+        return number;
+    }
+    else if (number > 0) {
+        return -number;
+    }
+    else {
+        return 0;
+    }
+}
+makeNegative(1);
+makeNegative(-5);
+makeNegative(0);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,29 @@ function isEvenOrOdd (number : number) : string {
 
 isEvenOrOdd(2);
 isEvenOrOdd(3);
+
+// Snack 2
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);  // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0);  // return 0
+
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(number : number) : number {
+    if (number < 0) {
+        return number;
+    } else if (number > 0){
+        return -number
+    } else {
+        return 0;
+    }
+}
+
+makeNegative(1);
+makeNegative(-5);
+makeNegative(0);
