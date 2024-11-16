565bdcbb2ce3365fecfdd7a66ed653b7bb05a51d
francesco-paolo-donofrio <geom.francescopaolo@gmail.com>
2024-11-16 22:51:55 +0100

    Instructions Snack 42

--- src/index.ts
+++ src/index.ts
@@ -938,3 +938,19 @@ function parseFloat(input: unknown): number | null {
   console.log(parseFloat(null));        // null
   console.log(parseFloat(undefined));   // null
   console.log(parseFloat("  123  "));   // 123
+
+//   Snack 42
+
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// toAlternatingCase("hello world") === "HELLO WORLD"
+// toAlternatingCase("HELLO WORLD") === "hello world"
+// toAlternatingCase("hello WORLD") === "HELLO world"
+// toAlternatingCase("HeLLo WoRLD") === "hEllO wOrld"
+// toAlternatingCase("12345")       === "12345"                   // Non-alphabetical characters are unaffected
+// toAlternatingCase("1a2b3c4d5e")  === "1A2B3C4D5E"
+// toAlternatingCase("String.prototype.toAlternatingCase") === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
