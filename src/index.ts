474bc54da1e209975d5c951113af1fe922ce762d
Frankosviz <geom.francescopaolo@gmail.com>
2024-12-01 20:45:47 +0100

    Snack 58 - Instructions

--- src/index.ts
+++ src/index.ts
@@ -1439,3 +1439,11 @@ function dontGiveMeFive(start: number, end: number): number {
 
 dontGiveMeFive(1, 9);
 dontGiveMeFive(1, 33);
+
+// Snack 58
+
+// Several people are standing in a row divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+// Task
+// Given an array of positive integers (the weights of the people), return a new array / tuple of two integers (depending on your language), whereby the first one is the total weight of team 1, and the second one is the total weight of team 2. Note that the array will never be empty.
+
