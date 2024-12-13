426442cbef9b271ae4c208000ce44f30f2c57825
Frankosviz <geom.francescopaolo@gmail.com>
2024-12-13 23:48:26 +0100

    Snack 88 - Sum the difference

--- src/index.ts
+++ src/index.ts
@@ -2172,3 +2172,25 @@ helloName("");
 
  whiskeyDiscount(10, 10, 500);
  whiskeyDiscount(12, 50, 1000);
+
+//  Snack 88
+
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+function sumTheDifferences(arr : number[]): number {
+    return arr.sort(function(a, b){return a - b}).reduce(reduceFunc);
+    function reduceFunc(firstValue : number, secondValue : number): number{
+        let result = firstValue - secondValue;
+        return result;
+    }
+}
+
+sumTheDifferences([2, 1, 10]);
