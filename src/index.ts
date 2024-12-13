d8538bb6b9c80e60379a57fe2879af74f58e1e51
Frankosviz <geom.francescopaolo@gmail.com>
2024-12-13 23:56:23 +0100

    Snack 88 - Sum the difference

--- src/index.ts
+++ src/index.ts
@@ -2185,14 +2185,6 @@ helloName("");
 
 // If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
 
-function sumTheDifferences(arr : number[]): number {
-    return arr.sort(function(a, b){return a - b}).reduce(reduceFunc);
-    function reduceFunc(firstValue : number, secondValue : number): number{
-        let result = firstValue - secondValue;
-        return result;
-    }
-}
-
 sumTheDifferences([2, 1, 10]);
 
 function sumTheDifferences(arr: number[]): number {
