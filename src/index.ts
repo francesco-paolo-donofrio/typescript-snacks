4fb63d31c3c3026478ee003afd65270e023b0bcb
francesco-paolo-donofrio <geom.francescopaolo@gmail.com>
2025-01-02 17:08:15 +0100

    Snack 104 - Cats in hats

--- src/index.ts
+++ src/index.ts
@@ -2573,3 +2573,25 @@ function sxor(n : number) : number {
 }
 
 sxor(5);
+
+// Snack 104
+
+// The Cat In The Hat has cat A under his hat, cat A has cat B under his hat and so on until Z
+
+// The Cat In The Hat is 2,000,000 cat units tall.
+
+// Each cat is 2.5 times bigger than the cat underneath their hat.
+
+// Find the total height of the cats if they are standing on top of one another.
+
+// Counting starts from the Cat In The Hat
+
+// n = the number of cats
+
+// fix to 3 decimal places.
+
+function height(n : number) : number {
+    return (2.5 ** n).toFixed(3);
+}
+
+
