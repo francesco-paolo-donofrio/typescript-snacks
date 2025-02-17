2adf5362da81575d8af7ebbd45e34df4c4d2e323
francesco-paolo-donofrio <geom.francescopaolo@gmail.com>
2025-02-17 15:33:21 +0100

    Snack 131 - Divisor and Bound

--- src/index.ts
+++ src/index.ts
@@ -3427,3 +3427,36 @@ function spot_diff(stringa1 : string, stringa2 : string): string | void {
 console.log(spotDiff("abcdefg", "abcqetg"));
 console.log(spotDiff("abcdefg", "abcdefq"));
 console.log(spotDiff("abcdefg", "abcdefg"));
+
+
+// Snack 131
+
+// Task
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+// Conditions :
+// N is divisible by divisor
+
+// N is less than or equal to bound
+
+// N is greater than 0.
+
+// Notes
+// The parameters (divisor, bound) passed to the function are only positive values .
+// It's guaranteed that a divisor is Found .
+// Input >> Output Examples
+// divisor = 2, bound = 7 ==> return (6)
+// Explanation:
+// (6) is divisible by (2) , (6) is less than or equal to bound (7) , and (6) is > 0 .
+
+// divisor = 10, bound = 50 ==> return (50)
+// Explanation:
+// (50) is divisible by (10) , (50) is less than or equal to bound (50) , and (50) is > 0 .*
+
+// divisor = 37, bound = 200 ==> return (185)
+// Explanation:
+// (185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+
+function divisibleBound(divisor : number, bound : number): number{
+    
+}
