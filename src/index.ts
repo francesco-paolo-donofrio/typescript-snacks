148245dca10326a73378af97e20d8e1240d89232
francesco-paolo-donofrio <geom.francescopaolo@gmail.com>
2025-02-24 15:39:19 +0100

    Snack 135 - Instructions + begin

--- src/index.ts
+++ src/index.ts
@@ -3581,3 +3581,29 @@ function returnCentury(numb : number): number {
 console.log(returnCentury(1705));
 console.log(returnCentury(0));
 console.log(returnCentury(1900));
+
+// Snack 135
+
+// This program tests the life of an evaporator containing a gas.
+
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+// Example:
+// evaporator(10, 10, 5) -> 29
+// Note:
+// Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
+
+function evaporator(content: number, evap_per_day: number, threshold: number): number {
+    content = 90;
+    evap_per_day = 7;
+    let mlLossEveryDay : number = content * evap_per_day / 100; 
+    let minimumMlToThreshold : number = content * threshold / 100;
+    let durationDays : number = 0;
+
+    for (let i = 0; i <= content; i++){
+        
+    }
+    
+}
