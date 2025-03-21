8461e9f6f5c97b5d47f050be7cfba09fdbeea495
francesco-paolo-donofrio <geom.francescopaolo@gmail.com>
2025-03-21 08:39:11 +0100

    Snack 150 - Instructions

--- src/index.ts
+++ src/index.ts
@@ -4382,3 +4382,12 @@ export function findEvenIndex(arr: number[]): number
 }
 
 console.log(findEvenIndex([1,2,3,4,3,2,1]));
+
+// Snack 150
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
