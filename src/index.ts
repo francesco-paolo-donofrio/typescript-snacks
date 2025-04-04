4fd85ad7a3ae5a124094bb0fa77c73ec4213cd4f
francesco-paolo-donofrio <geom.francescopaolo@gmail.com>
2025-04-04 09:34:03 +0200

    Snack 159 - Instructions

--- src/index.ts
+++ src/index.ts
@@ -4745,3 +4745,20 @@ function finddOutlier(integers: number[]): number | string {
 }
 
 finddOutlier([2, 4, 0, 100, 4, 11, 2602, 36]);
+
+// Snack 159
+
+// Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+export function stringTransformer(str: string) {
+    // Your code here
+  }
